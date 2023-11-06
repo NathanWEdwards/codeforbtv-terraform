7775e5ac94e9ea05b002ac6f703578830a1d506f
NathanWEdwards <Nathan.W.Edwards@Outlook.com>
2023-11-05 19:07:39 -0500

    Add firebase variables and providers.

---
+++ firebase/variables.tf
@@ -0,0 +1,8 @@
+variable "SECONDARY_STORAGE_BUCKET_ID" {
+  description = "(Optional) When the ID of a secondary bucket is supplied, one additional firebase storage bucket is provisioned."
+  type        = string
+}
+
+variable "GOOGLE_REGION" {
+  type = string
+}
